0bd701bdcaf188df038b753e18be8143dc541ce4
Bruno-Morais7 <brunomafonsomorais@gmail.com>
2022-04-10 23:47:23 +0000

    Random Card Dealer, without Feeling Confident part

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCardContent();
 };
+//document.getElementById("btn").addEventListener("click", randomCardContent());
+
+const suit = ["♥", "♦", "♣", "♠"];
+const nums = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function randomNumber() {
+  let randomNums = Math.floor(Math.random() * nums.length);
+  return randomNums;
+}
+function randomSuit() {
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return randomSuit;
+}
+
+let rdSuit = suit[randomSuit(suit)];
+let rdNums = nums[randomNumber(nums)];
+
+function randomCardContent() {
+  let symbols = document.getElementsByClassName("suit");
+  if (rdSuit == "♥" || rdSuit == "♦") {
+    symbols[0].style.color = "red";
+    symbols[1].style.color = "red";
+  } else {
+    symbols[1].style.color = "black";
+    symbols[0].style.color = "black";
+  }
+  symbols[0].innerHTML = rdSuit;
+  symbols[1].innerHTML = rdSuit;
+  document.getElementById("num").innerHTML = rdNums;
+}
